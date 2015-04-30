07fd696e7f19202c775279930083c44eadf2b05f
eduardboamba <eduard-andrei.boamba@sap.com>
2015-04-30 12:57:31 +0200

    no message

---
+++ js/reviewsController.js
@@ -0,0 +1,11 @@
+var reviewsModule = angular.module('reviewsModule', ["builder"]);
+
+ reviewsModule.controller('reviewsController', function ($scope, Restangular, currentAccountId, currentProjectId, notificationManager, linkManager) {
+
+     $scope.ratingsreviewsServiceUrl = Builder.currentWidget.settings.ratingsreviewsServiceUrl;
+     Restangular.setBaseUrl($scope.ratingsreviewsServiceUrl);
+
+     Restangular.all("/reviews").getList().then(function (response) {
+         $scope.reviews = Restangular.stripRestangular(response)
+     });
+ } );
